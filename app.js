d6988e21f99e9d33a1d2d5f6144936d13269bc1b
Tony Ng <tonlone@hotmail.com>
2023-03-28 10:07:33 -0400

    add tstorm for meteo

--- app.js
+++ app.js
@@ -131,6 +131,7 @@ window.addEventListener('load', ()=>{
             'rain': Skycons.RAIN,
             'psbl_rain': Skycons.RAIN,
             'rain_shower': Skycons.SHOWERS_DAY,
+            'tstorm': Skycons.THUNDER_RAIN,
             'thunderstorm': Skycons.THUNDER_RAIN,
             'local_thunderstorms': Skycons.THUNDER_SHOWERS_DAY,
             'light_snow': Skycons.SNOW,
